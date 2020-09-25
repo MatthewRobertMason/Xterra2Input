3cf13274f94ed7fb424aabfc0f20b176472420b1
MatthewRobertMason <Git@matthewmason.ca>
2020-09-25 03:18:13 -0300

    inital working demo.  a lot of interference in the wire if close to a power source.  button appears to remain 'held' in a game

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -28,13 +28,191 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.components = new System.ComponentModel.Container();
+            this.threshhold = new System.Windows.Forms.NumericUpDown();
+            this.inputDelay = new System.Windows.Forms.NumericUpDown();
+            this.threshold = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.micDb = new System.Windows.Forms.ProgressBar();
+            this.loopWorker = new System.ComponentModel.BackgroundWorker();
+            this.peakValue = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.keyComboBox = new System.Windows.Forms.ComboBox();
+            this.enableButtonPresses = new System.Windows.Forms.CheckBox();
+            ((System.ComponentModel.ISupportInitialize)(this.threshhold)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDelay)).BeginInit();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // threshhold
+            // 
+            this.threshhold.DecimalPlaces = 6;
+            this.threshhold.Increment = new decimal(new int[] {
+            2,
+            0,
+            0,
+            131072});
+            this.threshhold.Location = new System.Drawing.Point(106, 19);
+            this.threshhold.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.threshhold.Name = "threshhold";
+            this.threshhold.Size = new System.Drawing.Size(111, 20);
+            this.threshhold.TabIndex = 0;
+            this.threshhold.Value = new decimal(new int[] {
+            38,
+            0,
+            0,
+            131072});
+            // 
+            // inputDelay
+            // 
+            this.inputDelay.Location = new System.Drawing.Point(106, 45);
+            this.inputDelay.Maximum = new decimal(new int[] {
+            10000,
+            0,
+            0,
+            0});
+            this.inputDelay.Name = "inputDelay";
+            this.inputDelay.Size = new System.Drawing.Size(111, 20);
+            this.inputDelay.TabIndex = 1;
+            this.inputDelay.Value = new decimal(new int[] {
+            200,
+            0,
+            0,
+            0});
+            // 
+            // threshold
+            // 
+            this.threshold.AutoSize = true;
+            this.threshold.Location = new System.Drawing.Point(6, 21);
+            this.threshold.Name = "threshold";
+            this.threshold.Size = new System.Drawing.Size(54, 13);
+            this.threshold.TabIndex = 2;
+            this.threshold.Text = "Threshold";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(6, 47);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(83, 13);
+            this.label2.TabIndex = 3;
+            this.label2.Text = "Input Delay (ms)";
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.keyComboBox);
+            this.groupBox1.Controls.Add(this.label3);
+            this.groupBox1.Controls.Add(this.threshold);
+            this.groupBox1.Controls.Add(this.inputDelay);
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.threshhold);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(223, 100);
+            this.groupBox1.TabIndex = 4;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Input Configuration";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(6, 73);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(64, 13);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Key to Input";
+            // 
+            // micDb
+            // 
+            this.micDb.Location = new System.Drawing.Point(12, 162);
+            this.micDb.MarqueeAnimationSpeed = 1;
+            this.micDb.Maximum = 101;
+            this.micDb.Name = "micDb";
+            this.micDb.Size = new System.Drawing.Size(223, 23);
+            this.micDb.TabIndex = 5;
+            // 
+            // loopWorker
+            // 
+            this.loopWorker.WorkerReportsProgress = true;
+            this.loopWorker.DoWork += new System.ComponentModel.DoWorkEventHandler(this.backgroundWorker1_DoWork);
+            this.loopWorker.ProgressChanged += new System.ComponentModel.ProgressChangedEventHandler(this.backgroundWorker1_ProgressChanged);
+            this.loopWorker.RunWorkerCompleted += new System.ComponentModel.RunWorkerCompletedEventHandler(this.backgroundWorker1_RunWorkerCompleted);
+            // 
+            // peakValue
+            // 
+            this.peakValue.Enabled = false;
+            this.peakValue.Location = new System.Drawing.Point(118, 136);
+            this.peakValue.Name = "peakValue";
+            this.peakValue.Size = new System.Drawing.Size(117, 20);
+            this.peakValue.TabIndex = 6;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(10, 139);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(62, 13);
+            this.label4.TabIndex = 7;
+            this.label4.Text = "Peak Value";
+            // 
+            // keyComboBox
+            // 
+            this.keyComboBox.FormattingEnabled = true;
+            this.keyComboBox.Location = new System.Drawing.Point(106, 73);
+            this.keyComboBox.Name = "keyComboBox";
+            this.keyComboBox.Size = new System.Drawing.Size(111, 21);
+            this.keyComboBox.TabIndex = 5;
+            // 
+            // enableButtonPresses
+            // 
+            this.enableButtonPresses.AutoSize = true;
+            this.enableButtonPresses.Location = new System.Drawing.Point(13, 119);
+            this.enableButtonPresses.Name = "enableButtonPresses";
+            this.enableButtonPresses.Size = new System.Drawing.Size(133, 17);
+            this.enableButtonPresses.TabIndex = 8;
+            this.enableButtonPresses.Text = "Enable Button Presses";
+            this.enableButtonPresses.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
-            this.Text = "Form1";
+            this.ClientSize = new System.Drawing.Size(247, 195);
+            this.Controls.Add(this.enableButtonPresses);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.peakValue);
+            this.Controls.Add(this.micDb);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Xterra2Input";
+            ((System.ComponentModel.ISupportInitialize)(this.threshhold)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.inputDelay)).EndInit();
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
         }
 
         #endregion
+
+        private System.Windows.Forms.NumericUpDown threshhold;
+        private System.Windows.Forms.NumericUpDown inputDelay;
+        private System.Windows.Forms.Label threshold;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.ProgressBar micDb;
+        private System.ComponentModel.BackgroundWorker loopWorker;
+        private System.Windows.Forms.TextBox peakValue;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox keyComboBox;
+        private System.Windows.Forms.CheckBox enableButtonPresses;
     }
 }
 
--- Form1.cs
+++ Form1.cs
@@ -8,13 +8,129 @@ using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
+using NAudio.Wave;
+using NAudio.CoreAudioApi;
+
+using System.Runtime.InteropServices;
+//using VolumeControl.Library.Constants;
+//using VolumeControl.Library.Structs;
+
+
 namespace Xterra2Input
 {
     public partial class Form1 : Form
     {
+        float _dbLevel = 0;
+        float _threshold = 0.4f;
+
+        MMDeviceEnumerator deviceEnumerator;
+        MMDeviceCollection devices;
+        MMDevice microphone;
+
+        SendInputs si;
+
         public Form1()
         {
             InitializeComponent();
+
+            si = new SendInputs();
+            PopulateKeyList();
+            var waveIn = new WaveIn();
+            waveIn.StartRecording();
+            loopWorker.RunWorkerAsync();
+        }
+
+        private void backgroundWorker1_DoWork(object sender, DoWorkEventArgs e)
+        {
+            if (devices != null)
+            {
+                microphone = devices.FirstOrDefault();
+            }
+
+            if (microphone == null)
+            {
+                deviceEnumerator = new MMDeviceEnumerator();
+                devices = deviceEnumerator.EnumerateAudioEndPoints(DataFlow.Capture, DeviceState.Active);
+
+                if (devices != null)
+                {
+                    microphone = devices.FirstOrDefault();
+                }
+
+                if (microphone == null)
+                {
+                    Application.Exit();
+                }
+            }
+
+            while (microphone != null)
+            {
+                _dbLevel = microphone.AudioMeterInformation.MasterPeakValue;
+
+                if (_dbLevel > _threshold)
+                {
+                    loopWorker.ReportProgress((int)(_dbLevel * 100));
+                    System.Threading.Thread.Sleep((int)inputDelay.Value);
+                }
+                else
+                {
+                    loopWorker.ReportProgress(0);
+                }
+                System.Threading.Thread.Sleep(5);
+            }
+        }
+
+        private void backgroundWorker1_ProgressChanged(object sender, ProgressChangedEventArgs e)
+        {
+            if ((enableButtonPresses.Checked) && (keyComboBox.SelectedItem != null) && ((_dbLevel > _threshold)))
+            {
+                si.SendKeyInput(((keyComboBoxItem)keyComboBox.SelectedItem).value);
+                System.Threading.Thread.Sleep(50);
+                si.SendKeyUpInput(((keyComboBoxItem)keyComboBox.SelectedItem).value);
+            }
+
+            _threshold = (float)threshhold.Value;
+            peakValue.Text = string.Format("{0} - {1}", _dbLevel.ToString(), e.ProgressPercentage);
+
+            if (micDb.Value < e.ProgressPercentage)
+                micDb.Value = 101;
+            micDb.Value = Math.Min(101, Math.Max(0, e.ProgressPercentage));
+        }
+
+        private void backgroundWorker1_RunWorkerCompleted(object sender, RunWorkerCompletedEventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void PopulateKeyList()
+        {
+            //foreach (SendInputs.ScanCodeShort keyCode in (SendInputs.ScanCodeShort[]) Enum.GetValues(typeof(SendInputs.ScanCodeShort)))
+            //{
+            //    keyComboBox.Items.Add(new keyComboBoxItem() { name = Enum.GetName(typeof(SendInputs.ScanCodeShort), keyCode), value = keyCode });
+            //}
+
+            SendInputs.ScanCodeShort[] scanCodes = (SendInputs.ScanCodeShort[])Enum.GetValues(typeof(SendInputs.ScanCodeShort));
+
+            foreach (string keyName in Enum.GetNames(typeof(SendInputs.ScanCodeShort)))
+            {
+                if ((keyName.Length >= 4) && (keyName.Substring(0, 4) == "KEY_"))
+                {
+                    keyComboBox.Items.Add(new keyComboBoxItem() { name = keyName, value = scanCodes.Where(p => p.ToString() == keyName).FirstOrDefault() });
+                }
+            }
+
+            keyComboBox.Sorted = true;
+        }
+
+        internal class keyComboBoxItem
+        {
+            internal string name;
+            internal SendInputs.ScanCodeShort value;
+
+            public override string ToString()
+            {
+                return name;
+            }
         }
     }
 }
